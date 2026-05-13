a8f033f8e160effb0cfd090fdad2f1218160621e
joykon85 <iankoh@ymail.com>
2026-05-13 09:54:09 +0800

    feat: add partition for view rolling 7 days

--- src/nyc_forecasting/bigquery_sql/view_latest_prediction_metrics_rolling_7d.sql
+++ src/nyc_forecasting/bigquery_sql/view_latest_prediction_metrics_rolling_7d.sql
@@ -3,5 +3,6 @@ CREATE OR REPLACE VIEW `nyc-uber-494107.nyc_forecasting.vw_latest_prediction_met
 SELECT *
 FROM `nyc-uber-494107.nyc_forecasting.prediction_metrics_rolling_7d`
 QUALIFY ROW_NUMBER() OVER (
+  PARTITION BY model_version
   ORDER BY window_end_timestamp DESC
 ) = 1;
